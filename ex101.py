22515372f6e9b15a14719117d03af66acd7433b3
Yu Yu Lwin <yulwin178@gmail.com>
2018-06-26 15:15:44 +0630

    changed to dog

--- ex101.py
+++ ex101.py
@@ -1,6 +1,6 @@
-tabby_rat = "\tI'm tabbed in."
-persian_rat = "I'm split\non a line."
-backslash_rat = "I'm \\ a \\ cat."
+tabby_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ cat."
 
 fat_rat = """
  I'll do a list:
@@ -9,8 +9,8 @@ fat_rat = """
  \t* Catnip\n\t* Grass
  """
 
- print(tabby_rat)
- print(persian_rat)
- print(backslash_rat)
- print(fat_rat)
+ print(tabby_dog)
+ print(persian_dog)
+ print(backslash_dog)
+ print(fat_dog)
 
