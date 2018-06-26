4061df96233f35374c3ef10d1a92e0a23b8d30ca
yulwin <39519192+yulwin@users.noreply.github.com>
2018-06-26 15:14:46 +0630

    reverted rat to cat

--- ex101.py
+++ ex101.py
@@ -1,6 +1,6 @@
-tabby_rat = "\tI'm tabbed in."
-persian_rat = "I'm split\non a line."
-backslash_rat = "I'm \\ a \\ cat."
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
 
 fat_rat = """
  I'll do a list:
@@ -9,8 +9,8 @@ fat_rat = """
  \t* Catnip\n\t* Grass
  """
 
- print(tabby_rat)
- print(persian_rat)
- print(backslash_rat)
- print(fat_rat)
+ print(tabby_cat)
+ print(persian_cat)
+ print(backslash_cat)
+ print(fat_cat)
 
